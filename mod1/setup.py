8f0fd1b3acc4472e8024c42468907652990ca4a6
doug y'barbo <doug.ybarbo@gmail.com>
2014-11-12 14:10:44 -0800

    added comments

--- mod1/setup.py
+++ mod1/setup.py
@@ -3,6 +3,18 @@ from Cython.Build import cythonize
 from Cython.Distutils import build_ext
 import numpy as NP
 
+"""
+this extension module can be imported like so:
+
+>>> import tpl as TPL
+
+and the functions w/in this namespace called like so
+
+>>> TPL.divide(567, 4)
+
+"""
+
+
 # create an Extension object with the appropriate name & sources
 ext_module = Extension("tpl", 
 		sources=["tpl.pyx", "lib/tpl.c"],
